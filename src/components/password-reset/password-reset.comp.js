023843ee4f21bb3b4fac12d08b0ac0466647a4dc
Mehul Oswal <mehuloswal21@gmail.com>
2021-05-29 22:18:32 +0530

    Added Login/Reset Form

--- src/components/password-reset/password-reset.comp.js
+++ src/components/password-reset/password-reset.comp.js
@@ -3,41 +3,38 @@ import React from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types'
 
-export const LoginForm = ({handleOnChange,handleOnSubmit,formSwitcher,email,pass}) => {
+export const ResetPassword = ({handleOnChange,formSwitcher,handleOnResetSubmit,email}) => {
     return (
         <Container>
             <Row>
                 <Col>
-                    <h1 className="text-center" style={{color:"#1e7994"}}>Client Login</h1>
+                    <h1 className="text-center" style={{color:"#1e7994"}}>Reset Password</h1>
                     <hr />
-                    <Form onSubmit={handleOnSubmit}>
+                    <Form onSubmit={handleOnResetSubmit}>
                         <Form.Group>
                             <Form.Label>Email Address</Form.Label>
                             <Form.Control type="email" name="email" onChange={handleOnChange} value={email}   placeholder="Enter email" required />
                         </Form.Group>
-                        <Form.Group>
-                            <Form.Label className="mt-3">Password</Form.Label>
-                            <Form.Control type="password" name="password" onChange={handleOnChange} value={pass}  placeholder="Enter password" required />
-                        </Form.Group>
+                        
 
-                        <Button type="submit" className="mt-4" > Login </Button>
+                        <Button type="submit" className="mt-4" > Reset Password </Button>
                     </Form>
                     <hr />
                 </Col>
             </Row>
             <Row>
                 <Col>
-                <a href="#!" onClick={()=>formSwitcher('reset')} className="text-decoration-none">Forgot Password?</a>
+                <a href="#!" onClick={()=>formSwitcher('login')} className="text-decoration-none">Login Now?</a>
                 </Col>
             </Row>
 
         </Container>
     )
 }
-LoginForm.propTypes={
+ResetPassword.propTypes={
     handleOnChange: PropTypes.func.isRequired,
-    handleOnSubmit: PropTypes.func.isRequired,
+    handleOnResetSubmit: PropTypes.func.isRequired,
     formSwitcher: PropTypes.func.isRequired,
     email: PropTypes.string.isRequired,
-    pass: PropTypes.string.isRequired,
+    
 }
--- src/pages/entry/Entry.js
+++ src/pages/entry/Entry.js
@@ -1,17 +1,56 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./style.css";
 import "../../components/login/Login.comp"
-import { Jumbotron } from 'react-bootstrap'
 import { LoginForm } from '../../components/login/Login.comp';
+import { ResetPassword } from '../../components/password-reset/password-reset.comp';
+
 
 
 export const Entry = () => {
+    const [email, setemail] = useState('');
+    const [password, setpassword] = useState('');
+    const [formLoad, setformLoad] = useState('login');
+
+    const handleOnChange = (e) =>{
+        
+        const{name,value}=e.target
+        switch(name){
+            case 'email':
+                setemail(value)
+                break
+            case 'password':
+                setpassword(value)
+                break
+
+                default:
+                    break
+        }
+    }
+    const handleOnSubmit = (e)=>{
+        e.preventDefault()
+        if(!email || !password){
+           return alert("Fill all the Credentials")
+        }
+        
+        //TODO call api to submit the form
+    }
+    const handleOnResetSubmit = (e)=>{
+        e.preventDefault()
+        if(!email ){
+           return alert("Please enter the email")
+        }
+    }
+    const formSwitcher=(formType)=>{
+        setformLoad(formType)
+    }
+
     return (
         <div className="entry-page bg-info">
-            <Jumbotron >
-                <LoginForm />
-            </Jumbotron>
-
+            <div className="bg-light jumbotron m-3 form-box">
+                {formLoad ==='login'&&<LoginForm handleOnChange={handleOnChange} handleOnSubmit={handleOnSubmit} formSwitcher={formSwitcher} email={email} pass={password} /> }
+            
+                {formLoad ==='reset'&&<ResetPassword handleOnChange={handleOnChange} handleOnResetSubmit={handleOnResetSubmit} formSwitcher={formSwitcher} email={email}/>}
+            </div>
         </div>
     )
 }
