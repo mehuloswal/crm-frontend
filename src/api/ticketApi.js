b4562a48ecc88cf051e0ff5c85d920c244bd0872
Mehul Oswal <mehuloswal21@gmail.com>
2021-06-22 15:38:38 +0530

    fetchAllTickets api integrated

--- src/api/ticketApi.js
+++ src/api/ticketApi.js
@@ -5,8 +5,7 @@ export const getAllTickets = () => {
     try {
       const result = await axios.get("http://localhost:3001/v1/ticket", {
         headers: {
-          Authorization:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1laHVsb3N3YWwyMUBnbWFpbC5jb20iLCJpYXQiOjE2MjM4NjEzMzcsImV4cCI6MTYyMzg2MjIzN30.tz9xG4reyXjkfPcwdU4QfhaqS3VHk2dWhgj8Pa-uvGg",
+          Authorization: sessionStorage.getItem("accessJWT"),
         },
       });
       resolve(result);
