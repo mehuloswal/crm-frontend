0978a4ed48ade34848afa77e99591bdb398d6ac3
Mehul Oswal <mehuloswal21@gmail.com>
2021-06-25 22:45:33 +0530

    Fetched Pending & Total Tickets

--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,43 +1,67 @@
-import React from 'react'
-import { Container,Row,Col, Button } from 'react-bootstrap'
-import { TicketTable } from '../../components/ticket-table/TicketTable.comp'
-import tickets from '../../assets/data/dummy-ticket.json'
-import { BreadCrumb } from '../../components/breadcrumb/BreadCrumb.comp'
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { TicketTable } from "../../components/ticket-table/TicketTable.comp";
+import { BreadCrumb } from "../../components/breadcrumb/BreadCrumb.comp";
 import { Link } from "react-router-dom";
-
+import { fetchAllTickets } from "../ticket-list/ticketsAction";
 
 export const Dashboard = () => {
-    return (
-        <div>
-            <Container>
-                <Row>
-                    <Col>
-                    <BreadCrumb pageName="Dashboard"/>
-                    </Col>
-                </Row>
+  const { tickets } = useSelector((state) => state.tickets);
+  const dispatch = useDispatch();
 
-                <Row>
-                    <Col className="text-center mt-5 mb-2">
-                        <Link to='/add-ticket'><Button variant="info" style={{fontSize:'2rem', padding:'10px 30px', color:'white'}}>Add New Ticket</Button></Link> 
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="text-center mb-2">
-                        <div>Total tickets: 50</div>
-                        <div>Pending tickets: 5</div>
-                    </Col>
-                </Row>
-                <Row className="mb-1">
-                    <Col className="mt-3">
-                        <h4>Recently added Tickets:</h4>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col className="recent-ticket">
-                        <TicketTable tickets={tickets}/>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    )
-}
+  useEffect(() => {
+    if (!tickets.length) {
+      dispatch(fetchAllTickets());
+    }
+  }, [tickets, dispatch]);
+
+  const pendingTickets = tickets.filter((row) => row.status !== "Closed");
+
+  const totalTickets = tickets.length;
+
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <BreadCrumb pageName="Dashboard" />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col className="text-center mt-5 mb-2">
+            <Link to="/add-ticket">
+              <Button
+                variant="info"
+                style={{
+                  fontSize: "2rem",
+                  padding: "10px 30px",
+                  color: "white",
+                }}
+              >
+                Add New Ticket
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="text-center mb-2">
+            <div>Total tickets: {totalTickets}</div>
+            <div>Pending tickets: {pendingTickets.length}</div>
+          </Col>
+        </Row>
+        <Row className="mb-1">
+          <Col className="mt-3">
+            <h4>Recently added Tickets:</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="recent-ticket">
+            <TicketTable tickets={tickets} />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
